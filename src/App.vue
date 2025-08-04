e29c40bedbb11e52d5f6414a97b2f263143a87a4
Irokoooo <Xinyi2436698411@gmail.com>
2025-08-04 13:24:28 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
+
 <script setup>
 import { RouterView } from 'vue-router'
-import Sleepy from '@/components/Sleepy.vue'
 </script>
 
 <template>
   <RouterView />
-  <Sleepy class="fixed bottom-4 right-4" />
 </template>
