18c7527e6d70e0cd86152d4f88894ae5598d1140
Irokoooo <Xinyi2436698411@gmail.com>
2025-08-04 12:39:05 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Sleepy from '@/components/Sleepy.vue'  // 新增这行
+
 </script>
+ <Sleepy class="fixed bottom-4 right-4" />
 
 <template>
   <RouterView />
